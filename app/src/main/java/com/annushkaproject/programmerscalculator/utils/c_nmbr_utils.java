5103eb3794f4b040bad1eec566025617d7013194
Font77 <vimalkg@gmail.com>
2021-03-24 12:33:44 +0530

    hksuletr marzin not ziro vrd

--- app/src/main/java/com/annushkaproject/programmerscalculator/utils/c_nmbr_utils.java
+++ app/src/main/java/com/annushkaproject/programmerscalculator/utils/c_nmbr_utils.java
@@ -4,11 +4,13 @@ public class c_nmbr_utils {
     public static final String[] dizit_names_array = {"ziro","vn","tuu","Three","four","five","siks","seven","et","nine","ten","zilevn","kvAlv","dblyu","Aksen","phen"} ;
     public static final String[] ples_value_sphiks = {"","ti","so","to","zo","lo","mo","ko","po","bo","ro","do","co","go"} ;
     public static String heks_nmbr_string_to_vrdsstring(String heks_nmbr_string) {
-        String nmbr_in_vrds = ""; char c; final int nmbr_string_length = heks_nmbr_string.length() ;
+        String nmbr_in_vrds = ""; char c; final int nmbr_string_length = heks_nmbr_string.length() ; int cur_dizit=-1;
         for (int i = 0; i < nmbr_string_length ; i++){
-            c = heks_nmbr_string.charAt(i);
-            nmbr_in_vrds = nmbr_in_vrds + dizit_names_array[Character.digit(c,16)] + ples_value_sphiks[nmbr_string_length-i-1] ;
-            if (i < nmbr_string_length-1) { nmbr_in_vrds = nmbr_in_vrds + " " ; }
+            c = heks_nmbr_string.charAt(i); cur_dizit = Character.digit(c,16) ;
+            if(cur_dizit > 0) {
+                nmbr_in_vrds = nmbr_in_vrds + dizit_names_array[cur_dizit] + ples_value_sphiks[nmbr_string_length - i - 1];
+                if (i < nmbr_string_length-1) { nmbr_in_vrds = nmbr_in_vrds + " " ; }
+            }
         };
         return nmbr_in_vrds ;
     }
